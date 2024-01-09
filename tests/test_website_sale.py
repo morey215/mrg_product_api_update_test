d6da69852738112b24ddd7b251e5cb032d501f63
Mario Roberto <mrgomez.dev@gmail.com>
2024-01-09 09:37:35 -0600

    Update test code

--- tests/test_website_sale.py
+++ tests/test_website_sale.py
@@ -1,10 +1,20 @@
-# from odoo.tests import common
+from odoo.tests.common import TransactionCase
+from unittest.mock import patch, MagicMock
 
+# Pruebas para el método get_products
+class TestProductTemplateGetProducts(TransactionCase):
 
-# class TestWebsiteSale(common.TransactionCase):
-#    def setUp(self):
-#        super(TestWebsiteSale, self).setUp()
-#        self.test_book = self.env['product.product'].search([])
-#
-#    def test_button_available(self):
-#        self.assertEqual(self.test_book, '30', 'el numero es incorrecto')
+    # Prueba exitosa: Mockeando la respuesta de la API
+    def test_get_products(self):
+        # Inicializa el modelo de producto
+        product_template = self.env['product.template']
+
+        # Mockea la función 'requests.get' para retornar un JSON específico
+        with patch('requests.get') as mock_get:
+            mock_get.return_value.json.return_value = {'products': [{'title': 'Product1', 'price': 10, 'images': ['image_url'], 'id': '123'}]}
+
+            # Llama al método que queremos probar
+            products = product_template.get_products()
+
+            # Afirmaciones sobre el resultado esperado
+            self.assertTrue(isinstance(products, list))
