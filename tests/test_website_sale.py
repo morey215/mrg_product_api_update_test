2d5343768f8663186afab17219725ca8a9cf9c37
Mario Roberto <mrgomez.dev@gmail.com>
2024-01-09 21:09:24 -0600

    test validation product == 30

--- tests/test_website_sale.py
+++ tests/test_website_sale.py
@@ -1,20 +1,44 @@
-from odoo.tests.common import TransactionCase
-from unittest.mock import patch, MagicMock
+# Importamos la clase de prueba y algunas utilidades de Odoo
+from unittest.mock import MagicMock, patch
 
-# Pruebas para el método get_products
-class TestProductTemplateGetProducts(TransactionCase):
+from odoo.tests import common
 
-    # Prueba exitosa: Mockeando la respuesta de la API
-    def test_get_products(self):
-        # Inicializa el modelo de producto
-        product_template = self.env['product.template']
 
-        # Mockea la función 'requests.get' para retornar un JSON específico
-        with patch('requests.get') as mock_get:
-            mock_get.return_value.json.return_value = {'products': [{'title': 'Product1', 'price': 10, 'images': ['image_url'], 'id': '123'}]}
+# Creamos nuestra propia clase de prueba y heredamos de common.TransactionCase
+class TestProductTemplate(common.TransactionCase):
+    # Este método se ejecuta antes de cada prueba
+    def setUp(self):
+        super(TestProductTemplate, self).setUp()
 
-            # Llama al método que queremos probar
-            products = product_template.get_products()
+        # Simulamos la respuesta de la API con 30 productos
+        self.mock_response = MagicMock()
+        self.mock_response.json.return_value = {
+            'products': [
+                {'id': i, 'title': f'Producto{i}', 'price': i, 'images': []} for i in range(30)
+            ]
+        }
 
-            # Afirmaciones sobre el resultado esperado
-            self.assertTrue(isinstance(products, list))
+        # Parcheamos (simulamos) la función 'requests.get' para que devuelva nuestra respuesta simulada
+        self.patcher = patch('requests.get', return_value=self.mock_response)
+        self.mock_requests_get = self.patcher.start()
+
+    # Este método se ejecuta después de cada prueba
+    def tearDown(self):
+        super(TestProductTemplate, self).tearDown()
+
+        # Detenemos el parche para restaurar la funcionalidad original de 'requests.get'
+        self.patcher.stop()
+
+    # Este es nuestro caso de prueba
+    def test_load_products(self):
+        # Llamamos a la función que obtiene y carga productos desde la API
+        self.env['product.template'].sudo().load_products()
+
+        # Verificamos que la función 'get_products' fue llamada correctamente
+        self.mock_requests_get.assert_called_once_with('https://dummyjson.com/products')
+
+        # Contamos cuántos productos hay en la base de datos
+        product_count = self.env['product.template'].search_count([])
+
+        # Verificamos que se hayan creado exactamente 30 productos
+        self.assertEqual(product_count, 30)
